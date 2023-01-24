38f28513d221a8605096503e1e5ec547b3171cd3
SlawekHupka <slawek@hupka.pl>
2023-01-24 13:21:23 +0100

    adding new taks

--- js/script.js
+++ js/script.js
@@ -26,9 +26,33 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTasks = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTasks(newTaskContent);
+    };
+
     const init = () => {
         render();
 
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
+
     init();
 }
